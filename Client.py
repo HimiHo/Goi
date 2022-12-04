e7e300e725745c4bcdb7bc1b5dfbb289c3044bcc
HimiHo <elitedangerousserg@gmail.com>
2022-12-04 21:41:27 +0500

    Test2

--- Client.py
+++ Client.py
@@ -1,8 +1,11 @@
 import socket
 
+HOST = "127.0.0.1"
+PORT = 53210
+
 def client_sock():
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_sock.connect(('127.0.0.1', 53210))
+    client_sock.connect((HOST, PORT))
     client_sock.sendall(b'Hello, world')
     data = client_sock.recv(1024)
     client_sock.close()
--- Server.py
+++ Server.py
@@ -1,8 +1,11 @@
 import socket
 
+HOST = "127.0.0.1"
+PORT = 53210
+
 def serv_sock():
     serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto = 0)
-    serv_sock.bind(("127.0.0.1", 53210))
+    serv_sock.bind((HOST, PORT))
     serv_sock.listen(10)
 
     client_sock, client_addr = serv_sock.accept()
